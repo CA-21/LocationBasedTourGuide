d90bdec67ce0d3a2d94cd74684c4a9ed29bf026f
matts310 <mattschwartz310@gmail.com>
2014-11-20 10:30:45 -0500

    added tour

---
+++ src/com/example/locationbasedtourguide/Tour.java
@@ -0,0 +1,13 @@
+package com.example.locationbasedtourguide;
+
+public class Tour implements Comparable<Tour> {
+	
+	private String name;
+
+	@Override
+	public int compareTo(Tour another) {
+		
+		return name.compareTo(another.name);
+	}
+
+}
