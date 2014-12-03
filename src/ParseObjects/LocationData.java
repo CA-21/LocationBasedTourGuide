e061a02ab175e7553d2695ec5154ce52a838f58b
matts310 <mattschwartz310@gmail.com>
2014-12-03 12:40:09 -0500

    Changed package

---
+++ src/ParseObjects/LocationData.java
@@ -0,0 +1,163 @@
+package ParseObjects;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap.CompressFormat;
+import android.util.Log;
+
+import com.example.locationbasedtourguide.LoginActivity;
+import com.parse.ParseClassName;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+
+@ParseClassName("LocationData")
+public class LocationData extends ParseObject{
+
+	private SaveCallback nameSavedLogger = new SaveCallback() {
+
+		@Override
+		public void done(ParseException arg0) {
+			Log.i(LoginActivity.TAG, "Saved Location Data with name " + LocationData.this.getName());
+		}
+
+	};
+
+	public final static String LOCAITON_CLASS = "LocationData",
+			TEXT_KEY = "text", IMAGE_KEY = "image" , AUDIO_KEY = "audio",
+			RADIUS_KEY = "raidus", ORDERING_ID_KEY = "ordering_id", GPS_KEY = "location",
+			OWNING_TOUR_KEY = "owning_tour", NAME_KEY="name", NO_NAME_VALUE = "no name provided";
+
+	//Arbitrary
+	private final static double DEFAULT_RADIUS = 10;
+
+	//This will get called by parse, DO NOT USE THIS CONSTRUCTOR
+	public LocationData(){
+		super();
+		//this.setName(NO_NAME_VALUE);
+//		this.saveInBackground(nameSavedLogger);
+	}
+
+	public LocationData(String name){
+		super();
+		this.setName(name);
+		this.saveInBackground(nameSavedLogger);
+	}
+
+	public LocationData(String name, double lat, double lon){
+		super();
+		this.setName(name);
+		this.setLocation(lat, lon);
+		this.setRadius(DEFAULT_RADIUS);
+
+		this.saveInBackground(nameSavedLogger);
+	}
+
+	public LocationData(String name, double lat, double lon, double radius){
+		super();
+		this.setName(name);
+		this.setLocation(lat, lon);
+		this.setRadius(radius);
+
+		this.saveInBackground(nameSavedLogger);
+	}
+
+	public LocationData(String name, double lat, double lon, double radius, String text){
+		super();
+		this.setName(name);
+		this.setLocation(lat, lon);
+		this.setRadius(radius);
+		this.setText(text);
+		this.saveInBackground(nameSavedLogger);
+	}
+
+	public LocationData(String name, double lat, double lon, String text){
+		super();
+		this.setName(name);
+		this.setLocation(lat, lon);
+		this.setRadius(DEFAULT_RADIUS);
+		this.setText(text);
+		this.saveInBackground(nameSavedLogger);
+	}
+
+	public final double getLongitude(){
+		return this.getParseGeoPoint(GPS_KEY).getLongitude();
+	}
+
+	public final double getLatitude(){
+		return this.getParseGeoPoint(GPS_KEY).getLatitude();
+	}
+
+	public final double getRadius(){
+		return this.getDouble(RADIUS_KEY);
+	}
+
+	//private SOUNDTHING sound
+
+	public boolean hasText(){
+		return this.has(TEXT_KEY);
+	}
+	public boolean hasImage(){
+		return this.has(IMAGE_KEY);
+	}
+	//public abstract boolean hasAudio();
+
+	public String getText(){
+		return this.getString(TEXT_KEY);
+	}
+
+	public String getName(){
+		return this.getString(NAME_KEY);
+	}
+
+	//public Byte[] getAudio();
+	public Bitmap getImage(){
+		if(this.hasImage()){
+			return BitmapFactory.decodeByteArray(this.getBytes(IMAGE_KEY),0,0);
+		} else {
+			return null;
+		}
+	}
+
+	public void setText(String text){
+		this.put(TEXT_KEY, text);
+	}
+
+	//public abstract void setAudio(Byte[] input);
+
+	public void setImage(Bitmap input){
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		input.compress(CompressFormat.PNG, 100, stream);
+		byte[] toRet = stream.toByteArray();
+		this.put(IMAGE_KEY, toRet);
+		try {
+			stream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void setLocation(double lat, double lon){
+		ParseGeoPoint geoPoint = new ParseGeoPoint(lat, lon);
+		this.put(this.GPS_KEY,geoPoint);
+		
+	}
+
+	public void setName(String name){
+		this.put(NAME_KEY, name);
+	}
+
+	public void setRadius(double i ){
+		this.put(RADIUS_KEY,i);
+	}
+
+	public static ParseQuery<LocationData> getQuery(){
+		return ParseQuery.getQuery(LocationData.class);
+	}
+}
---
+++ src/ParseObjects/Tour.java
@@ -0,0 +1,210 @@
+package ParseObjects;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap.CompressFormat;
+import android.os.AsyncTask;
+
+import com.parse.ParseClassName;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+
+@ParseClassName("Tour")
+public class Tour extends ParseObject implements Comparable<Tour>{
+
+	public final static String NAME_KEY = "name", LOCATIONS_KEY = "Locations", IMAGE_KEY = "image";
+
+	//save this as an array when serializing out
+	private ArrayList<LocationData> mLocations;
+
+	/**
+	 * DO NOT CALL THIS DIRECTLY. USE TourGettingAndCreator
+	 * @param name
+	 */
+	public Tour(){
+		super();
+	}
+	/**
+	 * DO NOT CALL THIS DIRECTLY. USE TourGettingAndCreator
+	 * @param name
+	 */
+	public Tour(String name){
+		this.add(NAME_KEY,  name);
+		mLocations = new ArrayList<LocationData>();
+	}
+
+	public Tour(String name, ArrayList<LocationData> locations){
+		this.add(NAME_KEY, name);
+		this.mLocations = locations;
+	}
+
+	/*
+	 * returns the location data that was already present with the same
+	 * key, otherwise null
+	 */
+	public void addLocation(LocationData data){
+		mLocations.add(data);
+	}
+
+	public void addLocation(LocationData data, int position){
+		mLocations.add(position,data);
+
+	}
+
+	/*
+	 * returns the location removed, null if no location existed with ID i
+	 */
+	public LocationData removeLocation(int i){
+		if(i >= mLocations.size())
+			return null;
+
+		LocationData toRet = this.mLocations.remove(i);
+		this.mLocations.remove(i);
+		return toRet;
+	}
+
+	public void removeLocation(String id){
+
+	}
+
+	public String getName(){
+		return this.getString(NAME_KEY);
+	}
+
+	public String getAuthorName(){
+		return this.getParseUser("user").getUsername();
+	}
+
+	/*
+	 * returns the Location data if the id exists, null otherwise
+	 */
+	public LocationData getLocationData(int position ){
+		return this.mLocations.size() <= position ? null : mLocations.get(position);
+	}
+
+	public Bitmap getImage(){
+		Bitmap toRet = null;
+		if(this.has(IMAGE_KEY)){
+			ParseFile pf = this.getParseFile(IMAGE_KEY);
+			try {
+
+				toRet = BitmapFactory.decodeByteArray(pf.getData(),0,pf.getData().length);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} 
+		return toRet;
+	}
+
+	public void setImage(Bitmap input){
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		input.compress(CompressFormat.PNG, 100, stream);
+		byte[] toRet = stream.toByteArray();
+		ParseFile pf = new ParseFile("tourImage.png",toRet);
+		this.put(IMAGE_KEY, pf);
+		try {
+			stream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/** Returns all of the locationData. The returned list is a shallow copy
+	 * of the internal data, so modifications to the structure of the list itself
+	 * will not be retained. To modify the structure, use the appropriate add/remove
+	 * methods provided
+	 */
+	public ArrayList<LocationData> getAllLocationData(){
+		ArrayList<LocationData> toRet = new ArrayList<LocationData>();
+		for(LocationData ld : mLocations){
+			toRet.add(ld);
+		}
+		return toRet;
+	}
+
+	//Call this after downloading the tour
+	public void initializeTour(){
+		this.mLocations = (ArrayList) this.get(LOCATIONS_KEY);
+	}
+
+	@Override
+	public int compareTo(Tour another) {
+		return this.getName().compareTo(another.getName());
+	}
+
+	//Just compare the names
+	@Override
+	public boolean equals(Object o){
+		if(o == null)
+			return false;
+
+		if(o.getClass() != Tour.class){
+			return false;
+		}
+		return ((Tour)o).getName().equals(this.getName());
+	}
+
+	public void asyncSave(){
+		//Don't save if there are no locations
+		if(this.mLocations.size() != 0)
+			(new DataPersister()).execute();
+	}
+
+	public void asyncSave(SaveCallback saveCallback){
+		if(this.mLocations.size() != 0)
+			(new DataPersister()).execute(saveCallback);
+	}
+
+
+	public static ParseQuery<Tour> getQuery(){
+		return ParseQuery.getQuery(Tour.class);
+	}
+
+	/**
+	 * Saves the location data serially, then saves the overall tour in the background.
+	 * Needed to ensure that the location data is up to date before this tour is saved
+	 * @author matt
+	 *
+	 */
+	private class DataPersister extends AsyncTask<SaveCallback, Void, SaveCallback>{
+
+		@Override
+		protected SaveCallback doInBackground(SaveCallback... params) {
+			Tour.this.remove(LOCATIONS_KEY);
+			//			if(Tour.this.getList(LOCATIONS_KEY) != null)
+			//				Tour.this.removeAll(LOCATIONS_KEY, getList(LOCATIONS_KEY)); 	
+			try {
+				for(LocationData ld : mLocations){
+					ld.save();
+				}
+				Tour.this.addAll(LOCATIONS_KEY, Tour.this.mLocations);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+
+
+			return params.length == 0 ? null : params[0];
+		}
+
+		@Override
+		protected void onPostExecute(SaveCallback c){
+			if(c == null)
+				Tour.this.saveInBackground();
+			else 
+				Tour.this.saveInBackground(c);
+		}
+	}
+	
+	public int getNumberOfLocations(){
+		return this.mLocations.size();
+	}
+}
